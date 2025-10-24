c7a68552153309695da96b660bd16df68ee2ab67
Victor200524 <victor.donaire@hotmail.com>
2025-10-24 09:09:11 -0300

    Primeiro commit

---
+++ vagasonline/src/main/java/victor/trabalhovagasonline/vagasonline/VagasonlineApplication.java
@@ -0,0 +1,13 @@
+package victor.trabalhovagasonline.vagasonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VagasonlineApplication.class, args);
+    }
+
+}
---
+++ vagasonline/src/test/java/victor/trabalhovagasonline/vagasonline/VagasonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.trabalhovagasonline.vagasonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
